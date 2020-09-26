28a0fa15c28c81b57aab7d9146862fe001b1b7e0
Leon-Arno <leon.rimrod@gmail.com>
2020-09-27 01:39:49 +0200

    added topping relation

--- schemas/pizza.js
+++ schemas/pizza.js
@@ -1,3 +1,4 @@
+import { string } from 'prop-types';
 import { MdLocalPizza as icon } from 'react-icons/md';
 
 export default {
@@ -38,5 +39,30 @@ export default {
       validation: (Rule) => Rule.min(1000),
       // TODO: Add custom input component.
     },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+    },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // ? Filter underfined toppings out
+      const tops = Object.values(toppings).filter(
+        (topping) => topping !== undefined
+      );
+      // ? Return preview object for the pizza
+      // return { title, media, subtitle: Object.values(toppings).join(', ') };
+      return { title, media, subtitle: tops.join(', ') };
+    },
+  },
 };
